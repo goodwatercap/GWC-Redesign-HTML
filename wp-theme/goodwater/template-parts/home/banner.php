4a37c7b037654a431c4f2c32dc90cf7efdb388ef
Henry Lai <hhklai@gmail.com>
2025-05-27 10:54:29 -0600

    Comment out banner video

--- wp-theme/goodwater/template-parts/home/banner.php
+++ wp-theme/goodwater/template-parts/home/banner.php
@@ -1,16 +1,16 @@
 <div class="gwc banner banner--full-width">
     <div class="banner__inner">
         <div class="banner__wrap">
-            <video class="banner__video" autoplay="autoplay" loop muted playsinline> 
-                <?php $video_mp4 = get_field('video_mp4');
-                $video_webm      = get_field('video_webm');
-                if( $video_mp4 ): ?>
-                    <source src="<?php echo $video_mp4['url'];?>" type="video/mp4">
-                <?php endif;
-                if( $video_webm ):?>
-                    <source src="<?php echo $video_webm['url'];?>" type="video/webm">
-                <?php endif;?>
-            </video>
+<!--            <video class="banner__video" autoplay="autoplay" loop muted playsinline> -->
+<!--                --><?php //$video_mp4 = get_field('video_mp4');
+//                $video_webm      = get_field('video_webm');
+//                if( $video_mp4 ): ?>
+<!--                    <source src="--><?php //echo $video_mp4['url'];?><!--" type="video/mp4">-->
+<!--                --><?php //endif;
+//                if( $video_webm ):?>
+<!--                    <source src="--><?php //echo $video_webm['url'];?><!--" type="video/webm">-->
+<!--                --><?php //endif;?>
+<!--            </video>-->
             <div class="container">
                 <div class="banner__message">
                     <div class="banner__details">

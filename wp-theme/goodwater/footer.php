25557ee3d4dc39dea41b8b96e572687a56f2d919
Henry Lai <hhklai@gmail.com>
2025-06-20 11:25:43 -0600

    Add fraud warning when visitor is viewing with language set to korean.

--- wp-theme/goodwater/footer.php
+++ wp-theme/goodwater/footer.php
@@ -13,10 +13,32 @@
 <?php if( array_key_exists('footnote', $args )){
     $footnote = $args['footnote'];
 }?>
+    <dialog id="korea-warning">
+        <h3>사칭 범죄 유의 안내</h3>
+        <p>최근 당사(굿워터캐피탈)의 명칭, 로고, 소속 직원 등을 무단 도용한 사칭 범죄가 다수 발생하고 있습니다. 특히 투자 관련 리딩방 개설, 미끼용 웹사이트 접속 유도 또는 악성 앱 설치 등을 통해 투자금을 편취하는 사례가 확인되고 있으므로, 각별히 주의하여 주시기 바랍니다.</p>
+        <p>당사는 개인 명의 이메일, 문자메시지, SNS(텔레그램 등)를 통한 투자 권유, 불특정 다수 대상의 투자 리딩방 개설 및 초대, 외부 사이트나 앱 설치를 통한 투자 유도를 하지 않고 있으며, 위와 같은 사칭 범죄는 당사와 무관함을 알려드립니다.</p>
+        <p>만약 위와 같이 사칭이 의심되는 메시지나 링크를 수신하신 경우, 개인정보 및 금전피해를 방지하기 위해 절대 클릭하지 마시고, 당사(info@goodwatercap.com) 또는 경찰(112)로 신고 후 삭제해 주시기 바랍니다:</p>
+        <div>
+            <button class="l-btn l-btn--dark" type="submit" id="korea-warning-button">확인</button>
+        </div>
+    </dialog>
+    <script>
+      (function () {
+        if (navigator.language.startsWith('ko') && !localStorage.getItem('korea-warning-acked')) {
+          const dialog = document.getElementById("korea-warning");
+          const dialogClose = document.getElementById("korea-warning-button");
+
+          dialog.showModal();
+          dialogClose.addEventListener("click", () => {
+            localStorage.setItem('korea-warning-acked', "true");
+            dialog.close();
+          });
+        }
+      })();
+    </script>
 	<footer class="gwc footer">
         <div class="container container--lg">
             <?php echo $footnote; ?>
-
           	<div class="footer__brand">
             	<div class="footer__logo">
             		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Logo"><?php echo get_image(array('imgid'=>get_theme_mod('custom_logo'))); ?> </a>
